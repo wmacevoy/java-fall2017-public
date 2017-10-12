383e38e921e94405bda876512520ab00ebfabc00
Warren MacEvoy <wmacevoy@gmail.com>
2017-10-12 12:21:45 -0600

    wrapped sqlite

--- src/Sample.java
+++ src/Sample.java
@@ -10,43 +10,93 @@
  */
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Sample {
 
     public static void main(String[] args) {
-        Connection connection = null;
+        new Sample().run();
+    }
+
+    private Connection connection = null;
+
+    public Connection getConnection() {
+        if (connection == null) {
+            synchronized (this) {
+                if (connection == null) {
+                    try {
+                        connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+                    } catch (SQLException ex) {
+                        Logger.getLogger(Sample.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }
+        return connection;
+    }
+
+    void reset() {
         try {
-            // create a database connection
-            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            Connection connection = getConnection();
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
             statement.executeUpdate("drop table if exists person");
-            statement.executeUpdate("create table person (id integer, name string)");
-            statement.executeUpdate("insert into person values(1, 'leo')");
-            statement.executeUpdate("insert into person values(2, 'yui')");
-            ResultSet rs = statement.executeQuery("select * from person");
-            while (rs.next()) {
-                // read the result set
-                System.out.println("name = " + rs.getString("name"));
-                System.out.println("id = " + rs.getInt("id"));
-            }
-        } catch (SQLException e) {
-            // if the error message is "out of memory",
-            // it probably means no database file is found
-            System.err.println(e.getMessage());
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                // connection close failed.
-                System.err.println(e);
-            }
+            statement.executeUpdate("create table person (id integer primary key, name string)");
+        } catch (SQLException ex) {
+            Logger.getLogger(Sample.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    long insertPerson(String name) {
+        long id = -1;
+        try {
+            Connection connection = getConnection();
+            String sql = "insert into person (name) values (?)";
+
+            PreparedStatement ps
+                    = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, name);
+            ps.execute();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs != null && rs.next()) {
+                id = rs.getLong(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Sample.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return id;
+    }
+
+    String getPerson(long id) {
+        try {
+            Connection connection = getConnection();
+            String sql = "select name from person where id=?";
+            PreparedStatement ps
+                    = connection.prepareStatement(sql, Statement.NO_GENERATED_KEYS);
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs != null && rs.next()) {
+                return rs.getString("name");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Sample.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    void run() {
+        reset();
+        long aliceId = insertPerson("alice");
+        System.out.println("aliceId=" + aliceId);
+        long bobId = insertPerson("bob");
+        System.out.println("alice name=" + getPerson(aliceId));
+    }
+
 }
