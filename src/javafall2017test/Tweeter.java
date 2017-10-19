3e2e220bd04e299e656e9a60a23533ff451d89f9
Warren MacEvoy <wmacevoy@gmail.com>
2017-10-19 12:19:05 -0600

    fix tweeter format

--- src/javafall2017test/Tweeter.java
+++ src/javafall2017test/Tweeter.java
@@ -10,24 +10,27 @@ package javafall2017test;
  * @author wmacevoy
  */
 public class Tweeter {
+
     private class Job implements Runnable {
+
         private String user;
         private String message;
+
         Job() {
-            this.user=Tweeter.this.user;
-            this.message=Tweeter.this.message;
+            this.user = Tweeter.this.user;
+            this.message = Tweeter.this.message;
         }
 
         void tweet() {
-        try {
-        System.out.println("connecting to twitter...");
-        Thread.sleep(1000);
-        System.out.println("transferring message...");
-        Thread.sleep(1000);
-        System.out.println("done!");
-        System.out.println("tweeted: " + message + " for " + user);
-        } catch (InterruptedException ex) {            
-        }
+            try {
+                System.out.println("connecting to twitter...");
+                Thread.sleep(1000);
+                System.out.println("transferring message...");
+                Thread.sleep(1000);
+                System.out.println("done!");
+                System.out.println("tweeted: " + message + " for " + user);
+            } catch (InterruptedException ex) {
+            }
         }
 
         @Override
@@ -35,35 +38,38 @@ public class Tweeter {
             tweet();
         }
     }
-    
+
     private String user;
     private String message;
+
     public void setMessage(String _message) {
-        message=_message;
+        message = _message;
     }
+
     public void setUser(String _user) {
-        user=_user;
+        user = _user;
     }
-    
+
     public void tweetInForeground() {
         new Job().tweet();
     }
-    
+
     public void tweetInBackground() {
         Job job = new Job();
         Thread thread = new Thread(job);
         thread.start();
     }
-    
+
     public void tweetInForeground(String user, String message) {
-        synchronized(this) {
+        synchronized (this) {
             setUser(user);
             setMessage(message);
             tweetInForeground();
         }
     }
+
     public void tweetInBackground(String user, String message) {
-        synchronized(this) {
+        synchronized (this) {
             setUser(user);
             setMessage(message);
             tweetInBackground();
